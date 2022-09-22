e9c01864dd2389daae397933e63ce04dce8c04e4
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-09-22 18:43:06 -0300

    modificacion home y publicity

--- js/publicity.js
+++ js/publicity.js
@@ -1,11 +1,23 @@
 // Funcionalidad seleccionando la estrella
 
+const aceptar = document.querySelector('#agree');
+const negar = document.querySelector('#disclaim')
 const star = document.querySelector('#star');
 
-star.addEventListener('click',() => {
-    if (star.classList.contains("text-warning")){
-        star.classList.remove();
-        star.classList.add("fa-solid",  "fa-star",  "admin-icon-table");
-    }
-} )
+// star.addEventListener('click',() => {
+//     if (star.classList.contains("text-warning")){
+//         star.classList.remove();
+//         star.classList.add("fa-solid",  "fa-star",  "admin-icon-table");
+//         console.log(star.classList);
+//     }
+// } )
 
+star.addEventListener('click', cambiarEstilo);
+
+function cambiarEstilo(e){
+    if(e.target.classList.contains("text-warning")){
+        e.target.classList.replace('text-warning', "fa-solid", "fa-star", "admin-icon-table");
+    }else{
+        e.target.classList.add('text-warning');
+        }
+}
