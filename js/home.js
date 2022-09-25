98e747d1afe33e82857656fa0bc141f7dcab42f3
CintiaUtrera <cintiaa_utrera@outlook.es>
2022-09-24 23:20:33 -0300

    modificacion de publicidad

--- js/home.js
+++ js/home.js
@@ -1,10 +1,13 @@
 let publicidades= JSON.parse(localStorage.getItem("publicity")) || []
-let header = document.querySelector(".header")
+let header = document.getElementById("header")
 console.log(header)
 for (let i = 0; i < publicidades.length; i++) {
     const publicidad = publicidades[i];
-    if(publicidad.destacado===true){
-        header.style.backgroundImage = "url(`'${publicidad.img.src}'`)";
-        console.log(publicidad.img.src)
+    if (publicidad.destacado === true) {
+        header.style.backgroundImage = `url('${publicidad.img.src}')`;
+        header.style.height='500px'
+        header.style.backgroundSize='cover'
+        header.style.position='relative'
+        header.style.overflow='hidden'
     }
 }
