18b055abfaf12890a24cebd8be0d65039c66cf75
teraclitos <tefans12@gmail.com>
2022-09-26 18:03:30 -0300

    corrijo creacion de doctores aleatoria

--- js/data/doctors.js
+++ js/data/doctors.js
@@ -1,27 +1,27 @@
-
 const filterDoctors = (serviceId) => {
-  return fetchDoctors().filter(doctor => doctor.serviceId === serviceId);
-}
+  return fetchDoctors().filter((doctor) => doctor.serviceId === serviceId);
+};
 
 const fetchDoctors = () => {
-  return fetchItems('users').filter(user => user.role === 'Doctor');
-}
+  return fetchItems("users").filter((user) => user.role === "Doctor");
+};
 
-window.addEventListener('load', function(evt) {
+window.addEventListener("load", function (evt) {
   const doctors = fetchDoctors();
   if (doctors.length === 0) {
     const services = fetchServices();
 
-    for(let i = 0; i < services.length; i++) {
-      for(let j = 0; j < 3; j++) {
-        createItem('users', {
-          "userName": faker.name.firstName(),
-          "lastName": faker.name.lastName(),
-          "email": faker.internet.email(),
-          "role": "Doctor",
-          "professionalRegistration": faker.random.number(),
-          "password": faker.internet.password(),
-          "serviceId": services[i].id
+    for (let i = 0; i < services.length; i++) {
+      for (let j = 0; j < 3; j++) {
+        createItem("users", {
+          firstName: faker.name.firstName(),
+          lastName: faker.name.lastName(),
+          email: faker.internet.email(),
+          role: "Doctor",
+          matricula: faker.random.number(),
+          password: faker.internet.password(),
+          condition: "inactive",
+          serviceId: services[i].id,
         });
       }
     }
